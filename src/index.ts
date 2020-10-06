e5a85fa1efcbcbfc862f5cc2d426467e40f0541a
Kenta Kudo <yd37574@gmail.com>
2020-10-06 19:24:35 +0100

    berries

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
 import express from "express";
+import fetch from "node-fetch";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
     type Query {
-        hello: String
+        berries: [Berry!]!
+        berry(id: Int!): Berry
+    }
+
+    type Berry {
+        id: Int!
+        name: String!
+        Flavors: [BerryFlavorMap!]!
+    }
+
+    type BerryFlavorMap {
+        potency: Int!
+        flavor: NamedAPIResource!
+    }
+
+    type NamedAPIResource {
+        name: String!
+        url: String!
     }
 `);
 
+const baseURL = "https://pokeapi.co/api/v2";
+const berryEndpoint = `${baseURL}/berry`;
+
 const rootValue = {
-  hello: () => "Hello world!",
+  berries: () => {
+    return fetch(berryEndpoint)
+      .then((res) => res.json())
+      .catch(console.error);
+  },
+  berry: ({ id }: { id: number }) => {
+    return fetch(`${berryEndpoint}/${id}`)
+      .then((res) => res.json())
+      .catch(console.error);
+  },
 };
 
 const app = express();
