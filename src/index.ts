b30439dad4117ba40fda63f868abf6c688c14c1f
Kenta Kudo <yd37574@gmail.com>
2020-10-06 18:57:26 +0100

    graphql handler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const rootValue = {
+  hello: () => "Hello world!",
+};
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  })
+);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
